9e92315abb13d79b71b3a7f71adf30d4c9cc2574
severHatice <95497761+severHatice@users.noreply.github.com>
2023-09-24 21:20:09 +0200

    new changes

--- app.js
+++ app.js
@@ -6,24 +6,24 @@ let scoreContainer = document.querySelector("#score-container");
 let retryBtn = document.querySelector("#retry");
 let score = document.querySelector("#score");
 let minute = document.querySelector("#minute");
+let time = document.querySelector("#time");
 
 // variables for action
 
 let counterScore = 0;
 let counterTimer = 0;
 let shootingRepetation = 1000;
-let gameDuration =70000;
+let gameDuration = 10000;
 
 scoreContainer.style.display = "none";
 
 let clearInt = setInterval(() => {
-  gameStart();
+  startGame();
 }, shootingRepetation);
 
-function gameStart() {
-  // debugger;
-  const randomWidth = playgroung.clientWidth;
-  const randomHeight = playgroung.clientHeight;
+function startGame() {
+  const randomWidth = playgroung.clientWidth - target.clientWidth;
+  const randomHeight = playgroung.clientHeight - target.clientHeight;
 
   let randomX = Math.floor(Math.random() * randomWidth);
   let randomY = Math.floor(Math.random() * randomHeight);
@@ -31,13 +31,14 @@ function gameStart() {
   target.style.top = randomX + "px";
   target.style.left = randomY + "px";
 
-  target.addEventListener("click", () => {
+  target.addEventListener("click", clickedTarget);
+
+  function clickedTarget() {
     counterScore++;
-    target.style.background = "none";
-    target.classList.add("targetHiddenBorder");
-  });
+    target.style.classList.add("targetHiddenBorder");
+  }
 }
-//... sure icinde gameend metodu devreye girip oyun son bulacak
+
 setTimeout(() => {
   gameEnd();
 }, gameDuration);
@@ -45,38 +46,39 @@ setTimeout(() => {
 function gameEnd() {
   clearInterval(clearInt);
   // target.removeEventListener('click');
-  // ???
+  time.style.display = "none";
+
   scoreContainer.style.display = "block";
   target.style.display = "none";
   score.innerText = `${counterScore}`;
+}
+retryBtn.addEventListener("click", () => {
+  scoreContainer.style.display = "none";
+  target.style.display = "block";
+  counterScore = 0;
+  clearInt = setInterval(() => {
+    startGame();
+  }, shootingRepetation);
 
+  setTimeout(() => {
+    gameEnd();
+  }, gameDuration);
+});
+
+// timer part**********************************************************
+let totalSeconds = gameDuration / 1000;
+
+function getMinutesAndSeconds(seconds) {
+  let minutes = Math.floor(seconds / 60);
+  let remainingSeconds = seconds % 60;
+  return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
 }
 
-retryBtn.addEventListener("click", () => {
-    scoreContainer.style.display = "none";
-    target.style.display = "block";
-    counterScore = 0;
-    clearInt = setInterval(() => {
-      
-      gameStart();
-    }, shootingRepetation);
+const minuteDisplay = document.querySelector("#minute"); // Bu satırı dakika ve saniyenin görüntüleneceği DOM öğesine göre güncelleyin.
 
-    setTimeout(() => {
-      gameEnd();
-    }, gameDuration);
-  });
-// timer part**********************************************************
-let minut= ((gameDuration/1000)/60).toFixed(0);
-
-let second = (gameDuration/1000)%60;
-let minuteSecond= `${minut} : ${second}`
-// let minutes = gameDuration/1000;
-
-setInterval(()=>{
-  if(gameDuration >=0){
-    minute.innerText = `${minuteSecond}`;
-    minuteSecond--;
+setInterval(() => {
+  if (totalSeconds >= 0) {
+    minuteDisplay.innerText = getMinutesAndSeconds(totalSeconds);
+    totalSeconds--;
   }
-
-},1000);
-
+}, 1000);
